5a45f0c7be40fa890a9bf55dc6ec284215b0704d
Susmita-Dey <79099734+Susmita-Dey@users.noreply.github.com>
2021-11-01 21:14:58 +0530

    Create Day0_HelloWorld.cpp

---
+++ Day0_HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    string str;
+    getline(cin,str);
+    cout << "Hello, World.\n";
+    cout << str;
+    return 0;
+}
