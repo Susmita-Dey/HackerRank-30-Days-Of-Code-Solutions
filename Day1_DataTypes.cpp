5be34ad24376b8e0d3c8434f0ade2f85f8aa6153
Susmita-Dey <79099734+Susmita-Dey@users.noreply.github.com>
2021-11-02 10:04:29 +0530

    Add files via upload

---
+++ Day1_DataTypes.cpp
@@ -0,0 +1,38 @@
+/* Question Link:- https://www.hackerrank.com/challenges/30-data-types/problem */
+#include "bits\stdc++.h"
+using namespace std;
+
+int main()
+{
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+
+    // Declare second integer, double, and String variables.
+    int a;
+    double b;
+    string str;
+    // Read and save an integer, double, and String to your variables.
+    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+    cin >> a;
+    cin >> b;
+    getline(cin >> ws, str);
+    // Print the sum of both integer variables on a new line.
+    cout << i + a << endl;
+    // Print the sum of the double variables on a new line.
+    cout << d + b << endl;
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    cout << s + str << endl;
+    return 0;
+}
+
+/* Note:- 
+To consume the whitespace or newline between the end of a token 
+and the beginning of the next line:
+
+// eat whitespace
+getline(cin >> ws, s2);
+
+where s2 is a string. 
+*/
